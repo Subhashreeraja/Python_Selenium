ee0c51a42c0215cc6ea128bf21f953ac56ade8b8
Subhashree <subhashreerajait@gmail.com>
2026-06-09 22:49:12 +0530

    updated script

--- Data_Driven_Testing_Excel/Utilities/excelReader.py
+++ Data_Driven_Testing_Excel/Utilities/excelReader.py
@@ -1,16 +1,19 @@
+import os
 import openpyxl
 
-def get_data(path,sheet):
-    workbook = openpyxl.load_workbook(path)
-    sheet = workbook[sheet]
-    row = sheet.max_row
-    column = sheet.max_column
-
+def get_data(path, sheet_name):
     final_list = []
-    for r in range(2, row + 1):
+
+    base_dir = os.path.dirname(os.path.dirname(__file__))
+    file_path = os.path.join(base_dir, path)
+
+    workbook = openpyxl.load_workbook(file_path)
+    sheet = workbook[sheet_name]
+
+    for r in range(2, sheet.max_row + 1):
         row_list = []
-        for c in range(1, column + 1):
-            row_list.append(sheet.cell(r,c).value)
+        for c in range(1, sheet.max_column + 1):
+            row_list.append(sheet.cell(r, c).value)
         final_list.append(row_list)
-    
-    return final_list      
+
+    return final_list
