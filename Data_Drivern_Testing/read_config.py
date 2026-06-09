c8c5d5f8b2a71bbb3f59169b608394998fa949c5
Subhashree <subhashreerajait@gmail.com>
2026-06-09 22:47:46 +0530

    updated script

--- Data_Drivern_Testing/read_config.py
+++ Data_Drivern_Testing/read_config.py
@@ -1,6 +1,17 @@
-import pytest
 from configparser import ConfigParser
-def get_config(category,key):
-    config=ConfigParser()
-    config.read("./config.ini")
+import os
+
+def get_config(category, key):
+    config = ConfigParser()
+
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(base_dir, "config.ini")
+
+    print("LOADING:", file_path)
+
+    with open(file_path, "r", encoding="utf-8-sig") as f:
+        config.read_file(f)
+
+    print("SECTIONS:", config.sections())
+
     return config.get(category, key)
